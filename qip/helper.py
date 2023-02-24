00062d05eb6a4723454d0d6f3b77713ab7399777
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2023-02-25 02:13:30 +0900

    Complete implementation of cFRQI

--- qip/helper.py
+++ qip/helper.py
@@ -1,5 +1,5 @@
-from sympy import fwht 
 import numpy as np
+
 
 def sfwht(a):
     n = len(a)
@@ -15,6 +15,7 @@ def sfwht(a):
         j*=2
     return a            
 
+
 def isfwht(a):
     n = len(a)
     k = ilog2(n)
@@ -29,8 +30,10 @@ def isfwht(a):
         j*=2 
     return a            
 
+
 def ispow2(x):
     return not (x&x-1)
+
 
 def nextpow2(x):
     x-=1
@@ -43,11 +46,14 @@ def nextpow2(x):
     x+=1
     return x 
 
+
 def ilog2(x):
     return int(np.log2(x))
 
+
 def grayCode(x):
     return x^(x>>1)
+
 
 def grayPermutation(a):
     b = np.zeros(len(a))
@@ -55,18 +61,39 @@ def grayPermutation(a):
         b[i] = a[grayCode(i)]
     return b
 
+
 def invGrayPermutation(a):
     b = np.zeros(len(a))
     for i in range(len(a)):
         b[grayCode(i)] = a[i]
     return b
 
+
 def convertToAngles(a,maxval):
     pi2=2*np.arctan(1)
     scal = pi2/maxval 
     a = a *scal 
 
-def confertToGrayscale(a,maxval):
+
+def convertToGrayscale(a,maxval):
     pi2=2*np.arctan(1)
     scal = maxval/pi2 
     a = a * scal 
+
+
+def countr_zero(n, n_bits=8):
+    """Returns the number of consecutive 0 bits in the value of x, starting from the least significant bit ("right")."""
+    if n == 0:
+        return n_bits
+
+    counts = [0]
+    count = 0
+    while n != 0:
+        if n & 1 == 0:
+            count += 1
+        elif count > 1:
+            counts.append(count)
+            count = 0
+        n >>= 1
+
+    return max(counts)
